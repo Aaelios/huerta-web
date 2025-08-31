bd6c0344b25466db310a09e551f5ab9af0209de1
Roberto Huerta <rhuerta@myowngmail.com>
2025-08-30 18:45:04 -0600

    chore: ignore playground and scripts in repo/deploy

--- playground.ts
+++
@@ -1,3 +0,0 @@
-import { config } from 'dotenv'; config({ path: '.env.local' });
-import { f_buildReturnUrl } from './lib/checkout/f_buildReturnUrls.ts';
-console.log(f_buildReturnUrl()); // espera https://huerta-consulting.com/gracias?session_id={CHECKOUT_SESSION_ID}
